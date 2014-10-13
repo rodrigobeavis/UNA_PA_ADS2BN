7390542e62e3a3aff70ba7b926c7bd00ea3245be
user.mail=rodrigobeavis@yahoo.com.br <rodrigobeavis@yahoo.com.br>
2014-10-12 22:52:34 -0300

    PDO OK.

--- sistema/DAO/DaoAutenticacao.php
+++ sistema/DAO/DaoAutenticacao.php
@@ -11,44 +11,42 @@
  *
  * @author RODRIGO
  */
-class DaoAutenticacao {
-    
-//extends PDOConnectionFactory {
-//
-//    public $conex = null;
-//
-//    public function DAOPosVendas() {
-//        $this->conex = PDOConnectionFactory::getConnection();
-//    }
-    
 
-    public function localizarUser($codUser) {
+if (file_exists('./sistema/PDO/PDOConnectionFactory.php')) {
+    require_once('./sistema/PDO/PDOConnectionFactory.php');
+} else {
+    require_once('../PDO/PDOConnectionFactory.php');
+}
+
+class DaoAutenticacao extends PDOConnectionFactory {
+    
+    public $conex = null;
+
+    public function DaoAutenticacao() {
+        $this->conex = PDOConnectionFactory::getConnection();
+    }
+    public function localizarUser($dadosuser) {
+        var_dump($dadosuser);
         try {
-            $sql = "SELECT 
-                    idAutenticacao,
-                    nome,
-                    login,
-                    senha,
-                    tipoDeAcesso,
-                    regraAcesso,
-                    areaAcesso,
-                    id_Colaboradores
-                FROM
-                    tbl_autenticacao AS t1
-                WHERE
-                    StatusAcesso != ''
-                        AND StatusAcesso != NULL
-                        AND email = ?";
+        $sql = "SELECT 
+            COUNT(idAutenticacao)
+        FROM
+            tbl_autenticacao AS t1
+        WHERE
+            t1.tipoDeAcesso = 0
+                AND t1.email = ?
+                AND t1.senha = ?";
         $stmt = $this->conex->prepare($sql);
-        $stmt->bindValue(1, $codUser);
+        $stmt->bindValue(1, $dadosuser['email']);
+        $stmt->bindValue(2, $dadosuser['keyU']);
 
         $stmt->execute();
         return $stmt;
-            
-            
+       
         } catch (PDOException $e) {
             echo $e->getMessage();
         }
+        parent::Close();
     }
 
 }
--- sistema/PDO/PDOConnectionFactory.php
+++ sistema/PDO/PDOConnectionFactory.php
@@ -4,57 +4,33 @@
  * and open the template in the editor.
  */
 
-abstract class  PDOConnectionFactory {
-    //put your code here
-    // recebe a conexão
+class  PDOConnectionFactory extends PDO {
+
     public $con = null;
-    // qual o banco de dados?
-    public $dbType 	= "mysql";
-
-
-
-    // parâmetros de conexão
-    // quando não for necessário deixe em branco apenas com as aspas duplas ""
-    /*
-    public $host 	= "localhost";
-    public $user 	= "root";
-    public $senha 	= "";
-    public $db		= "teste";
-     *
-     */
-
-    // seta a persistência da conexão
+    public $dbType = "mysql";
     public $persistent = false;
 
-    // new PDOConnectionFactory( true ) <--- conexão persistente
-    // new PDOConnectionFactory()       <--- conexao não persistente
-    public function PDOConnectionFactory( $persistent=false ){
-            // verifico a persistência da conexao
+    public function PDOConnectionFactory( $persistent = false ){
             if( $persistent != false){ $this->persistent = true; }
     }
 
     public function getConnection(){
                     try{
-
-                        if(file_exists("configs/config.ini")){
-                            $cfg = parse_ini_file("configs/config.ini");
-                        } elseif(file_exists("../configs/config.ini")) {
-                            $cfg = parse_ini_file("../configs/config.ini");
-							} else {
-								$cfg = parse_ini_file("/var/www/htdocs/siao/configs/config.ini");
-							}
-
-                            // realiza a conexão
-                            $this->con = new PDO($this->dbType.":host=".$cfg['host'].";dbname=".$cfg['db'], $cfg['user'], $cfg['pwd'],
+                        if(file_exists("./sistema/configs/config.ini")){
+                            $cfg = parse_ini_file("./sistema/configs/config.ini");
+                        } elseif(file_exists("sistema/configs/config.ini")) {
+                            $cfg = parse_ini_file("sistema/configs/config.ini");
+				} else {
+				$cfg = parse_ini_file("/var/www/htdocs/PA/sistema/configs/config.ini");
+				}
+                            $this->con = new PDO($this->dbType.":host=".$cfg['host'].";dbname=".$cfg['dbname'], $cfg['user'], $cfg['password'],
                             array( PDO::ATTR_PERSISTENT => $this->persistent ) );
-                            // realizado com sucesso, retorna conectado
+                            var_dump($this->con);
                             return $this->con;
-                    // caso ocorra um erro, retorna o erro;
-                    }catch ( PDOException $ex ){  echo "Erro: ".$ex->getMessage(); }
+                    }catch ( PDOException $ErroMysql ){  echo "Erro: ".$ErroMysql->getMessage(); }
 
     }
 
-    // desconecta
     public function Close(){
         if( $this->con != null) {
             $this->con = null;
